8e2eec1a6d4470c59ec41bb3fb44ee3c112c4d16
Valuriel <christian.poetter@hotmail.de>
2020-11-04 12:51:39 +0200

    added actor objects and level characters from Kalle's plus level instance

--- game.js
+++ game.js
@@ -1,3 +1,92 @@
+// Implementing Actor objects ro represent the current position and state of a given moving element in the game
+class Vec {
+    constructor(x, y) {
+        this.x = x; this.y = y;
+    }
+    plus(other) {
+        return new Vec(this.x + other.x, this.y + other.y);
+    }
+    time(factor) {
+        return new Vec(this.x * factor, this.y * factor);
+    }
+}
+
+// Player actor
+
+class Player {
+    constructor(pos, speed) {
+        this.pos = pos; // the "pos" property holds the coordinates of the element's top-left corner
+        this.speed = speed; // the "speed" property stores the current speed of the player class
+    }
+    get type() { return "player"; }
+
+    static create(pos) {
+        return new Player(pos.plus(new Vec(0, -0.5)),
+        new Vec(0, 0));
+    }
+}
+
+Player.prototype.size = new Vec(0.8, 1.5);
+
+// Lava actor creation
+
+class Lava {
+    constructor(pos, speed, reset) {
+        this.pos = pos;
+        this.speed = speed;
+        this.reset = reset;
+    }
+    get type() { return "lava"; }
+
+    static create(pos, ch) {
+        if (ch == "=") {
+            return new Lava(pos, new Vec(2, 0));
+        } 
+        else if (ch == "|") {
+            return Lava(pos, new Vec(0, 2));
+        }
+        else if (ch == "v") {
+            return new Lava(pos, new Vec(0, 3), pos);
+        }
+    }
+}
+
+Lava.prototype.size = new Vec(1, 1);
+
+// Coin actor creation
+
+class Coin {
+    constructor(pos, basePos, wobble) {
+        this.pos = pos;
+        this.basePos = basePos;
+        this.wobble = wobble;
+    }
+    get type() { return "coin"; }
+
+    static create(pos) {
+        let basePos = pos.plus(new Vec(0.2, 0.1));
+        return new Coin(basePos, basePos, Math.random() * Math.PI * 2);
+    }
+}
+
+Coin.prototype.size = new Vec(0.6, 0.6);
+
+
+// level characters
+
+const levelChars = {
+    '.': 'empty', '#': 'wall', '+': 'lava',
+    '@': Player, 'o': Coin,
+    '=': Lava, '|': Lava, 'v': Lava
+}
+
+// creating a level instance
+
+let simpleLevel = new Level(simpleLevelPlan);
+console.log(`${simpleLevel.width} by ${simpleLevel.height}`);
+// --> 22 by 9
+
+
 // Player motion and regulations when the player hits a wall
 
 const playerXSpeed = 7;
@@ -29,7 +118,7 @@ Player.prototype.update = function(time, state, keys) {
     return new Player(pos, new Vec(xSpeed, ySpeed));
 };
 
-// Key handler for keeping the control keys "pressed" for continuos movement
+// Key handler for keeping the control keys "pressed" for continuous movement
 function trackKeys(keys) {
     let down = Object.create(null);
     function track(event) {
