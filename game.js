1b3a19088058a0a06f4df2dd5902a007069cd11f
Artem Zagarov <artem.zagarov@gmail.com>
2020-11-04 12:50:48 +0200

    added Level Reading

--- game.js
+++ game.js
@@ -72,3 +72,53 @@ function elt(name, attrs, ...children) {
       this.dom.scrollTop = center.y + margin - height;
     }
   };
+
+//Reading Level 
+
+class Level {
+  constructor(plan) {
+    let rows = plan.trim().split('\n').map(l => [...l])
+    this.height = rows.length
+    this.width = rows[0].length
+    this.startActors = []
+
+    this.rows = rows.map((row, y) => {
+      return row.map((ch, x) => {
+        let type = levelChars[ch]
+        if (typeof type == 'string') return type
+        this.startActors.push(
+          type.create(new Vec(x, y), ch))
+        return 'empty'
+      })
+    })
+  }
+}
+
+class State {
+  constructor(level, actors, status) {
+    this.level = level
+    this.actors = actors
+    this.status = status
+  }
+
+  static start(level) {
+    return new State(level, level.startActors, 'playing')
+  }
+
+  get player() {
+    return this.actors.find(a => a.type == 'player')
+  }
+}
+
+class Vec {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+  plus(other) {
+    return new Vec(this.x + other.x, this.y + other.y)
+  }
+  times(factor) {
+    return new Vec(this.x * factor, this.y * factor)
+  }
+}
