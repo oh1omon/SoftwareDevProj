c19013cf771d409fe185c3786683b22c471e4324
MattiHel <57216448+MattiHel@users.noreply.github.com>
2020-12-02 11:05:00 +0200

    Add monster to level one to test if it works

--- game.js
+++ game.js
@@ -119,12 +119,40 @@ var Coin = class Coin {
 
 Coin.prototype.size = new Vec(0.6, 0.6);
 
+//Adding monsters
+const monsterSpeed = 4;
+
+class Monster {
+  constructor(pos) { this.pos = pos; }
+
+  get type () { return "monster"; }
+  static create(pos) { return new Monster (pos.plus(new Vec (0, -1))); }
+
+  update(time, state) {
+    let player = state.player;
+    let speed = (player.pos.x < this.pos.x ? -1 : 1) * time * monsterSpeed;
+    let newPos = new Vec(this.pos.x + speed, this.pos.y);
+    if (state.level.touches(newPos, this.size, "wall")) return this;
+    else return new Monster(newPos);
+  }
+
+  collide(state) {
+    let player = state.player;
+    if (player.pos.y + player.size.y < this.pos.y + 0.5) {
+      let filtered = state.actors.filter(a => a != this );
+      return new State(state.level, state.actors, "lost");
+    }
+  }
+}
+
+Monster.prototype.size = new Vec(1.2, 2);
+
 // level characters
 
 const levelChars = {
   ".": "empty", "#": "wall", "+": "lava",
   "@": Player, "o": Coin,
-  "=": Lava, "|": Lava, "v": Lava
+  "=": Lava, "|": Lava, "v": Lava, "M": Monster
 };
 
 // creating a level instance
@@ -340,6 +368,8 @@ Player.prototype.update = function(time, state, keys) {
     return new Player(pos, new Vec(xSpeed, ySpeed));
 };
 
+
+
 //******** Player controls ************/
 
 // Key handler for keeping the control keys "pressed" for continuous movement
@@ -476,3 +506,4 @@ function runAnimation(frameFunc) {
     }
 
 // runGame(GAME_LEVELS, DOMDisplay);
+
