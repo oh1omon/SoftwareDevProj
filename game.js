0a0040f9f23e54ee2d275f2a6a673b93835524c0
MattiHel85 <70198997+MattiHel85@users.noreply.github.com>
2020-12-16 11:18:47 +0200

    Update game.js

--- game.js
+++ game.js
@@ -120,26 +120,29 @@ var Coin = class Coin {
 Coin.prototype.size = new Vec(0.6, 0.6);
 
 //Adding monsters
-const monsterSpeed = 4;
+const monsterSpeed = 1.5;
 
 class Monster {
   constructor(pos) { this.pos = pos; }
 
   get type () { return "monster"; }
   static create(pos) { return new Monster (pos.plus(new Vec (0, -1))); }
-
+  
   update(time, state) {
-    let player = state.player;
-    let speed = (player.pos.x < this.pos.x ? -1 : 1) * time * monsterSpeed;
-    let newPos = new Vec(this.pos.x + speed, this.pos.y);
-    if (state.level.touches(newPos, this.size, "wall")) return this;
-    else return new Monster(newPos);
+        let player = state.player;
+        let speed = (.pos.x < this.pos.x ? -1 : 1) * time * monsterSpeed;
+        let newPos = new Vec(this.pos.x + speed, this.pos.y);
+        if (state.level.touches(newPos, this.size, "wall")) 
+          return this;
+        else return new Monster(newPos);
   }
 
   collide(state) {
     let player = state.player;
     if (player.pos.y + player.size.y < this.pos.y + 0.5) {
       let filtered = state.actors.filter(a => a != this );
+      return new State(state.level, state.actors, state.status);
+    } else {
       return new State(state.level, state.actors, "lost");
     }
   }
